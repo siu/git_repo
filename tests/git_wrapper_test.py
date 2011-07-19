bfe52a8c82b2e48b3f03c0398378493a0e20f6b7
David Siñuela Pastor <siu.4coders@gmail.com>
2011-07-19 16:26:39 +0200

    Little refactor on git_wrapper_test.py

--- tests/git_wrapper_test.py
+++ tests/git_wrapper_test.py
@@ -5,14 +5,17 @@ import git_wrapper
 from git_wrapper import GitWrapper
 from mock import Mock
 
-class GitWrapperTest(unittest.TestCase):
+class GitWrapperBaseTest(unittest.TestCase):
     def mock_git_cmd(self, return_value):
         self.git_cmd = Mock()
         self.git_cmd.return_value = return_value
         git_wrapper.execute_git_cmd = self.git_cmd
 
-    def test_paths(self):
+class GitWrapperTest(GitWrapperBaseTest):
+    def setUp(self):
         self.repo = GitWrapper('bogus_repo')
+
+    def test_paths(self):
         self.mock_git_cmd("sample_file.txt\ntest/test_a.py")
 
         self.assertEqual(self.repo.paths, ['sample_file.txt', 'test/test_a.py'])
@@ -21,7 +24,6 @@ class GitWrapperTest(unittest.TestCase):
                 self.repo.path)
 
     def test_stage_new_files(self):
-        self.repo = GitWrapper('bogus_repo')
         self.mock_git_cmd("?? sample_file.txt\n?? second_sample_file.txt")
 
         self.assertEqual(self.repo.stage, {
@@ -33,7 +35,6 @@ class GitWrapperTest(unittest.TestCase):
                 self.repo.path)
 
     def test_stage_modified_file(self):
-        self.repo = GitWrapper('bogus_repo')
         self.mock_git_cmd(" M sample_file.txt")
 
         self.assertEqual(self.repo.stage, { 'sample_file.txt': 'M' })
@@ -41,8 +42,11 @@ class GitWrapperTest(unittest.TestCase):
         self.git_cmd.assert_called_with('--git-dir=.git status --porcelain',
                 self.repo.path)
 
-    def test_paths_different_git_dir(self):
+class GitWrapperTestExternalGitDir(GitWrapperBaseTest):
+    def setUp(self):
         self.repo = GitWrapper('bogus_repo', '../bogus_repo.git')
+
+    def test_paths_different_git_dir(self):
         self.mock_git_cmd("sample_file.txt\ntest/test_a.py\n")
 
         assert self.repo.paths
