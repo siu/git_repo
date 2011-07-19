5acfa920c20fc18ad07015aa00fe449be3d0a893
David Siñuela Pastor <siu.4coders@gmail.com>
2011-07-19 15:20:51 +0200

    Rename fixtures to simpler names, allow .git folder to live outside the work-tree

--- tests/git_wrapper_integration_tests.py
+++ tests/git_wrapper_integration_tests.py
@@ -4,21 +4,21 @@ from git_wrapper import GitWrapper
 
 class GitWrapperIntegrationTest(util.RepoTestCase):
     def test_paths(self):
-        self.open_tar_repo('fixture_project01')
+        self.open_tar_repo('project01')
         assert('test_file.txt' in self.repo.paths)
         assert('hello_world.rb' in self.repo.paths)
 
     def test_stage(self):
-        self.open_tar_repo('fixture_project02')
+        self.open_tar_repo('project02')
         assert('not_committed_file.txt' in self.repo.stage)
         assert('second_not_committed_file.txt' in self.repo.stage)
 
-#    def test_paths_external_git_folder(self):
-#        self.open_tar_repo('fixture_project03', '../fixture_project03.git')
-#        assert('test_file.txt' in self.repo.paths)
-#        assert('hello_world.rb' in self.repo.paths)
-#
-#    def test_stage_external_git_folder(self):
-#        self.open_tar_repo('fixture_project04', '../fixture_project04.git')
-#        assert('not_committed_file.txt' in self.repo.stage)
-#        assert('second_not_committed_file.txt' in self.repo.stage)
+    def test_paths_external_git_folder(self):
+        self.open_tar_repo('project03', '../project03.git')
+        assert('test_file.txt' in self.repo.paths)
+        assert('hello_world.rb' in self.repo.paths)
+
+    def test_stage_external_git_folder(self):
+        self.open_tar_repo('project04', '../project04.git')
+        assert('not_committed_file.txt' in self.repo.stage)
+        assert('second_not_committed_file.txt' in self.repo.stage)
